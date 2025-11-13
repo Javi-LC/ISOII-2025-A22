5edf6d04a7e230585a0a2c791ac29ca18dd6273d
Aitor-RM <147514156+Aitor42@users.noreply.github.com>
2025-11-13 16:56:42 +0000

    Correction in project skeleton

--- project/Component_20/src/main/java/org/Component_20/App.java
+++
@@ -1,7 +0,0 @@
-package org.Component_20;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- project/Component_24/src/main/java/org/Component_24/App.java
+++
@@ -1,7 +0,0 @@
-package org.Component_24;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
