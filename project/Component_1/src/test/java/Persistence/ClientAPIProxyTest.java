c0ff276aa320692b6cd3e84dceb882017077ed18
Javier López Cabrera <javier.lopez76@alu.uclm.es>
2025-11-21 22:59:38 +0100

    Component_1_Client_Testing_Phase

---
+++ project/Component_1/src/test/java/Persistence/ClientAPIProxyTest.java
@@ -0,0 +1,22 @@
+package Persistence;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientAPIProxyTest {
+
+    @Test
+    void fetchCourses_shouldBeTested() {
+        fail("Not yet implemented: test for ClientAPIProxy.fetchCourses");
+    }
+
+    @Test
+    void fetchMaterials_shouldBeTested() {
+        fail("Not yet implemented: test for ClientAPIProxy.fetchMaterials");
+    }
+
+    @Test
+    void fetchSearch_shouldBeTested() {
+        fail("Not yet implemented: test for ClientAPIProxy.fetchSearch");
+    }
+}
