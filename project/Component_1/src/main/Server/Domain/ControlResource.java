2aac60139316ac1a8b0c80919650047e0487b2e8
Javier López Cabrera <javier.lopez76@alu.uclm.es>
2025-11-21 22:49:46 +0100

    Component_1_Server_Implementation_Phase

---
+++ project/Component_1/src/main/Server/Domain/ControlResource.java
@@ -0,0 +1,14 @@
+package Domain.;
+
+public class ControlResource {
+
+	/**
+	 * 
+	 * @param query
+	 */
+	public Resource[] findByQuery(string query) {
+		// TODO - implement ControlResource.findByQuery
+		throw new UnsupportedOperationException();
+	}
+
+}
