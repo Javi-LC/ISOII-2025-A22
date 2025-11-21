0e0168cf933e79be4d3c6208a16ec85dfa698d78
Javier López Cabrera <javier.lopez76@alu.uclm.es>
2025-11-21 22:55:31 +0100

    Component_1_Client_Implementation_Phase

---
+++ project/Component_1/src/main/Client/Domain/Resource.java
@@ -0,0 +1,13 @@
+package Domain;
+
+public class Resource {
+
+    public String id;
+    public String title;
+
+    public Resource(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+}
