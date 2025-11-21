b4cf4ed86f679b231585e30b162671e26dba3dc5
Sardor Kurbonmurodov <sardor.kurbonmurodov1@alu.uclm.es>
2025-11-21 23:28:24 +0000

    Component_12_Server_Implementation_Phase

--- project/Component_12/src/main/java/org/Component_12/App.java
+++ project/Component_12/src/main/java/org/Component_12/App.java
@@ -1,7 +1,7 @@
-package org.Component_12;
+package org.Component_12.server.presentation;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Server App started");
   }
 }
