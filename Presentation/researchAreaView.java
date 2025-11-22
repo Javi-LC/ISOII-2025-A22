aa0ae3ec39c1bac534704cff02175038a4db3ec6
magal <magal@example.com>
2025-11-22 01:52:16 +0100

    Component_7_Client_Implementation_Phase

---
+++ Presentation/researchAreaView.java
@@ -0,0 +1,29 @@
+package Presentation.client;
+
+public class researchAreaView {
+
+	public void displayProjects() {
+		// TODO - implement researchAreaView.displayProjects
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param resId
+	 */
+	public void requestSubscription(int resId) {
+		// TODO - implement researchAreaView.requestSubscription
+		throw new UnsupportedOperationException();
+	}
+
+	public void showSuccess() {
+		// TODO - implement researchAreaView.showSuccess
+		throw new UnsupportedOperationException();
+	}
+
+	public void showError() {
+		// TODO - implement researchAreaView.showError
+		throw new UnsupportedOperationException();
+	}
+
+}
