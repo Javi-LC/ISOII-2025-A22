acee49ee1cac732583fb201e7defe4a249ca097a
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-11-22 01:33:58 +0100

    Component_4_Client_Testing_Phase

---
+++ project/Component_4/src/main/Client/Domain/ControlTracker.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class ControlTracker {
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public Material getMaterial(String name) {
+		// TODO - implement ControlTracker.getMaterial
+		throw new UnsupportedOperationException();
+	}
+
+}
