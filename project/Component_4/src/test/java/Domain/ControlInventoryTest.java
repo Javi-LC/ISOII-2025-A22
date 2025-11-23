16dad4d8e3759a148eee71233ccfc8f9d7a3e64f
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-11-23 02:19:16 +0100

    Component_4_Testing_Phase

---
+++ project/Component_4/src/test/java/Domain/ControlInventoryTest.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class ControlInventoryTest {
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public boolean manageInventory(String name) {
+		// TODO - implement ControlInventory.manageInventory
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ project/Component_4/src/test/java/Domain/ControlRecordsTest.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class ControlRecordsTest {
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public boolean modifyRecords(String name) {
+		// TODO - implement ControlRecords.modifyRecords
+		throw new UnsupportedOperationException();
+	}
+
+}
