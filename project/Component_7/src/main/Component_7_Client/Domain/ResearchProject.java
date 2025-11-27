e875cebfffcee7ac552d07d353d922ae769b08fa
marcosgr5 <marcos.galan@alu.uclm.es>
2025-11-27 19:45:17 +0100

    Add files via upload

---
+++ project/Component_7/src/main/Component_7_Client/Domain/ResearchProject.java
@@ -0,0 +1,27 @@
+package org.Component_7_Client.Domain;
+
+public class ResearchProject {
+    private int id;
+    private String name;
+
+    public ResearchProject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
