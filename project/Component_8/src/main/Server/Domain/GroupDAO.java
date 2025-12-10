6c7b7e71a756b1af93824d0a8b6990384c025a09
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-12-10 16:15:34 +0100

    Component_8_Server_Implementation_Phase

---
+++ project/Component_8/src/main/Server/Domain/GroupDAO.java
@@ -0,0 +1,55 @@
+package Domain;
+
+public class GroupDAO {
+
+	/**
+	 * 
+	 * @param groupName
+	 */
+	public boolean createGroup(String groupName) {
+		// TODO - implement GroupDAO.createGroup
+		throw new UnsupportedOperationException();
+	}
+
+	public Group getGroups() {
+		// TODO - implement GroupDAO.getGroups
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public boolean modifyGroup(int groupId) {
+		// TODO - implement GroupDAO.modifyGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public boolean deleteGroup(int groupId) {
+		// TODO - implement GroupDAO.deleteGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public Group findGroupById(int groupId) {
+		// TODO - implement GroupDAO.findGroupById
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public Group findGroupByName(String name) {
+		// TODO - implement GroupDAO.findGroupByName
+		throw new UnsupportedOperationException();
+	}
+
+}
