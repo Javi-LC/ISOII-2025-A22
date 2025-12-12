15ae6f833a44f92f7cc6adb6cfff2b485a5f147b
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-12-12 21:56:07 +0100

    Component_8_Server_Testing_Phase

---
+++ project/Component_8/src/test/Client/Domain/StudentTest.java
@@ -0,0 +1,10 @@
+package Domain;
+
+public class StudentTest {
+
+	private int studentId;
+	private String name;
+	private String birthdate;
+	private String study;
+
+}
---
+++ project/Component_8/src/test/Client/Presentation/UIGroupViewerTest.java
@@ -0,0 +1,49 @@
+package Presentation;
+
+import Domain.*;
+
+public class UIGroupViewerTest {
+
+	public void showGroups() {
+		// TODO - implement UIGroupViewer.showGroups
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 * @param members
+	 */
+	public boolean createGroup(String name, int[] members) {
+		// TODO - implement UIGroupViewer.createGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public Group getGroup(int groupId) {
+		// TODO - implement UIGroupViewer.getGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public boolean modifyGroup(int groupId) {
+		// TODO - implement UIGroupViewer.modifyGroup
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param groupId
+	 */
+	public boolean deleteGroup(int groupId) {
+		// TODO - implement UIGroupViewer.deleteGroup
+		throw new UnsupportedOperationException();
+	}
+
+}
