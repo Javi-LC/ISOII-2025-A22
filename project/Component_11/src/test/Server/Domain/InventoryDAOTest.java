69c4ac3bcce769f9cbef3e66908eaf42ce09b446
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-12-15 19:42:20 +0100

    Component_11_Server_Testing_Phase

---
+++ project/Component_11/src/test/Server/Domain/InventoryDAOTest.java
@@ -0,0 +1,37 @@
+package Domain;
+
+public class InventoryDAOTest {
+
+	public Inventory getInventory() {
+		// TODO - implement InventoryDAO.getInventory
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public boolean insertMaterial(String name) {
+		// TODO - implement InventoryDAO.insertMaterial
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public boolean deleteMaterial(String name) {
+		// TODO - implement InventoryDAO.deleteMaterial
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public boolean updateMaterial(String name) {
+		// TODO - implement InventoryDAO.updateMaterial
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ project/Component_11/src/test/Server/Domain/MaterialDAOTest.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class MaterialDAOTest {
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public Material getMaterial(String name) {
+		// TODO - implement MaterialDAO.getMaterial
+		throw new UnsupportedOperationException();
+	}
+
+}
