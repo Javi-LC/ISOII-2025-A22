d1c7b7c7e8d03812d104659e5e8f46b99e69efaf
RubenCalzadoC <ruben.calzado@alu.uclm.es>
2025-12-15 19:47:54 +0100

    Component_11_Client_Testing_Phase

---
+++ project/Component_11/src/test/Client/Persistance/AgentTest.java
@@ -0,0 +1,12 @@
+package Persistance;
+
+import java.net.Proxy;
+
+public class AgentTest {
+
+	public Proxy getProxy() {
+		// TODO - implement Agent.getProxy
+		throw new UnsupportedOperationException();
+	}
+
+}
