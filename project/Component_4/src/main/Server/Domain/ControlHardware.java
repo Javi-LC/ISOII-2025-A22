6b10bfc7acafbeb60230ec6c6615edadafcc90b6
Jesús Alises <jesus10.jar@hotmail.com>
2025-12-17 12:56:53 +0100

    Updated the server Design

---
+++ project/Component_4/src/main/Server/Domain/ControlHardware.java
@@ -0,0 +1,20 @@
+package Domain;
+
+public class ControlHardware {
+
+	/**
+	 * 
+	 * @param reserveDate
+	 * @param userId
+	 */
+	public void processReserve(int reserveDate, int userId) {
+		// TODO - implement ControlHardware.processReserve
+		throw new UnsupportedOperationException();
+	}
+
+	public void createReserve() {
+		// TODO - implement ControlHardware.createReserve
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ project/Component_4/src/main/Server/Presentation/LibraryAPIServer.java
@@ -0,0 +1,15 @@
+package Presentation;
+
+public class LibraryAPIServer {
+
+	/**
+	 * 
+	 * @param token
+	 * @param jsonBody
+	 */
+	public void handleRequest(int token, int jsonBody) {
+		// TODO - implement LibraryAPIServer.handleRequest
+		throw new UnsupportedOperationException();
+	}
+
+}
